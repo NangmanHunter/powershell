8bd2b48bc671344229acae519f9ccc57fbf7391f
djwlfdls1 <djwlfdls1@gmail.com>
2025-02-23 01:44:59 +0900

    bac

---
+++ .res/nav.js
@@ -0,0 +1,7 @@
+document.getElementById("nav").innerHTML=
+`<ul>
+    <li><a href="/doc/01.cmdlet.html">cmdlet</a></li>
+    <li><a href="/doc/01.cmdlet.html">cmdlet</a></li>
+    <li><a href="/doc/02.function.html">function</a></li>
+    <li><a href="/doc/03.alias.html">alias</a></li>
+</ul>`;
